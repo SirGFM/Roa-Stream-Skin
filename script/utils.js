96650c337bd0c769135c6d7c62018eb1c5fa5109
SirGFM <gfmandaji@hotmail.com>
2018-02-10 14:32:07 -0200

    Refactor speedrun's bootstrap so it's cleaner and more maintainable

--- script/utils.js
+++ script/utils.js
@@ -118,3 +118,177 @@ function parseURL(url) {
 
     return ret;
 }
+
+/**
+ * If necessary, increases the value by 1 so it becomes even.
+ *
+ * @param{val} Value to be modified.
+ */
+function makeValueEven(val) {
+    return val + (val % 2);
+}
+
+/**
+ * Centers an object horizontally.
+ *
+ * @param{elementName} Name of the element to be centralized
+ * @param{parentName} Element that should be used for centering and for the
+ *                    horizontal offset. The element must no be parented to this!
+ *                    If empty, the entire document is used.
+ * @param{newWidth} New width of the object. If not set, defaults to offsetWidth
+ * @param{forceEven} Forces every value to be even values
+ * @param{maxViewWidth} Cap the view's width to this value (useful for
+ *                      subsections)
+ */
+function centerDomHorizontal(elementName, parentName, newWidth, forceEven, maxViewWidth) {
+    let left = 0;
+    let viewLeft = 0;
+    let viewWidth = 0;
+    let element = document.getElementById(elementName);
+
+    if (parentName) {
+        let parentEl = document.getElementById(parentName);
+
+        viewLeft = parentEl.offsetLeft;
+        viewWidth = parentEl.offsetWidth;
+    }
+    else {
+        viewLeft = document.body.offsetLeft;
+        viewWidth = document.body.offsetWidth;
+    }
+
+    if (!newWidth) {
+        newWidth = element.offsetWidth;
+    }
+    if (maxViewWidth && viewWidth > maxViewWidth) {
+        viewWidth = maxViewWidth;
+    }
+
+    left = viewLeft + Math.floor((viewWidth - newWidth) * 0.5);
+
+    if (forceEven) {
+        left += left % 2;
+        newWidth += newWidth % 2;
+    }
+
+    element.style.left = left+"px";
+    element.style.width = newWidth+"px";
+}
+
+/**
+ * Centers an object vertically.
+ *
+ * @param{elementName} Name of the element to be centralized
+ * @param{parentName} Element that should be used for centering and for the
+ *                    vertical offset. The element must no be parented to this!
+ *                    If empty, the entire document is used.
+ * @param{newHeight} New height of the object. If not set, defaults to offsetHeight
+ * @param{forceEven} Forces every value to be even values.
+ * @param{maxViewHeight} Cap the view's height to this value (useful for
+ *                      subsections)
+ */
+function centerDomVertical(elementName, parentName, newHeight, forceEven, maxViewHeight) {
+    let elTop = 0;
+    let viewTop = 0;
+    let viewHeight = 0;
+    let element = document.getElementById(elementName);
+
+    if (parentName) {
+        let parentEl = document.getElementById(parentName);
+
+        viewTop = parentEl.offsetTop;
+        viewHeight = parentEl.offsetHeight;
+    }
+    else {
+        viewTop = document.body.offsetTop;
+        viewHeight = document.body.offsetHeight;
+    }
+
+    if (!newHeight) {
+        newHeight = element.offsetHeight;
+    }
+    if (maxViewHeight && viewHeight > maxViewHeight) {
+        viewHeight = maxViewHeight;
+    }
+
+    elTop = viewTop + Math.floor((viewHeight - newHeight) * 0.5);
+
+    if (forceEven) {
+        elTop += elTop % 2;
+        newHeight += newHeight % 2;
+    }
+
+    element.style.top = elTop+"px";
+    element.style.height = newHeight+"px";
+}
+
+/**
+ * Position an element (presumably a line) within the parent. The object's
+ * relative offset (as it's expected to have 'absolute' positioning) is
+ * set from 'pos' (either top or bottom).
+ *
+ * @param{elementName} Name of the element to be centralized
+ * @param{pos} Relative position (either 'top' or 'bottom')
+ * @param{offset} Offset from the relative position
+ * @param{widthDiff} How much should the width differ from its parent's.
+ */
+function positionDivLine(elementName, pos, offset, widthDiff) {
+    let element = document.getElementById(elementName);
+    let newWidth = element.parentElement.offsetWidth;
+
+    element.style[pos] = offset+"px";
+    centerDomHorizontal(elementName, element.parentElement.id, newWidth+widthDiff, true);
+    element.style.left = Math.floor(widthDiff * -0.5)+"px";
+}
+
+/**
+ * Position a fixed element just bellow another
+ *
+ * @param{elementName} Name of the element to be positioned
+ * @param{parentName} Element that should be used for positioning. The element
+ *                    must no be parented to this! If empty, the entire document
+ *                    is used.
+ * @param{offset} Distance from the parent object.
+ * @param{forceEven} Forces every value to be even values.
+ */
+function setDomPositionBellow(elementName, parentName, offset, forceEven) {
+    let elTop = 0;
+    let element = document.getElementById(elementName);
+
+    if (parentName) {
+        let parentEl = document.getElementById(parentName);
+
+        elTop = parentEl.offsetTop;
+        elTop += parentEl.offsetHeight;
+    }
+    else {
+        elTop = document.body.offsetTop;
+        elTop += document.body.offsetHeight;
+    }
+    elTop += offset;
+
+    if (forceEven) {
+        elTop += elTop % 2;
+    }
+
+    element.style.top = elTop+"px";
+}
+
+/**
+ * Position an element (presumably a line) within the parent. The object's
+ * relative offset (as it's expected to have 'absolute' positioning) is
+ * set from 'pos' (either top or bottom).
+ *
+ * @param{elementName} Name of the element to be centralized
+ * @param{pos} Relative position (either 'top' or 'bottom')
+ * @param{offset} Offset from the relative position
+ * @param{widthDiff} How much should the width differ from its parent's.
+ */
+function positionDivLine(elementName, pos, offset, widthDiff) {
+    let element = document.getElementById(elementName);
+    let newWidth = element.parentElement.offsetWidth;
+
+    element.style[pos] = offset+"px";
+    centerDomHorizontal(elementName, element.parentElement.id, newWidth+widthDiff, true);
+    element.style.left = Math.floor(widthDiff * -0.5)+"px";
+}
