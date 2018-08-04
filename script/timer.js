a9edaed7467a7bc0c4f11c771e96503201cfd0b0
SirGFM <gfmandaji@hotmail.com>
2018-08-04 13:23:02 -0300

    Refactor timer callback to 'onHold' from 'onPress'
    Previosly, the callback could easily get called and activated
    because of a miss-press. Also, the timming on multi-presses was
    quite tight, to avoid miss-presses.
    
    Making the callback depend on holding the button for sometime
    (currently, 500ms) fixes thoses issues two issues.
    
    The delay introduced by checking the button for some time is
    removed from the accumulated time. This does cause the timer to
    move 500ms back when unpaused.

--- script/timer.js
+++ script/timer.js
@@ -1,9 +1,18 @@
 // Maximum time allowed between multi-presses
-//const _multiPress = 150;
-// Set a tighter time so l2 doesn't trigger this... D:
-const _multiPress = 100;
+const _multiPress = 200;
 // Amount of presses required for a multi-press
-const _multiPressRequired = 2;
+const _resetTimerCount = 3;
+// How long a button must be held to be considered a press
+const _pressDetectionMs = 500;
+// Last time when the button wasn't pressed (for measuring pressed time)
+let _lastReleased = 0;
+// To detect when the button was last pressed
+let _lastState = false;
+// When the button was actually pressed
+let _pressTime = 0;
+// Whether the press has been handled
+let _handle = false;
+
 // Time when the last callback was called
 let _lastCallback = 0;
 // Amount of presses since the last single press
@@ -33,42 +42,57 @@ function setupTimer(element) {
 
 /**
  * Callback used to trigger reseting, pausing and unpausing the timer.
+ *
+ * To solve issues caused by miss-pressing the button, it ignores presses
+ * shorter _pressDetectionMs.
  */
-function timerCallback() {
+function timerCallback(state) {
     let _now = Date.now()
 
-    // Detect whether it's a multipress or not
-    if (_now - _lastCallback > _multiPress) {
-        _multiPressCount = 1;
+    if (state) {
+        if (!_lastState) {
+            // Detect whether it's a multipress or not
+            if (_now - _lastCallback > _multiPress) {
+                _multiPressCount = 1;
+                _pressTime = Date.now();
+            }
+            else {
+                _multiPressCount++;
+            }
+
+            _lastCallback = _now;
+        }
+
+        // Check whether the button has been held for long enough
+        if (!_handled && Date.now() - _lastReleased >= _pressDetectionMs) {
+            // Do actually take an action based on the button press.
+            handleTimerCallback();
+            _handled = true;
+        }
     }
     else {
-        _multiPressCount++;
+        _lastReleased = Date.now();
+        _handled = false;
     }
-
-    // Set a callback so multi-presses may be detected
-    if (!_pressHandle) {
-        _pressHandle = window.setTimeout(handleTimerCallback, _multiPress*(_multiPressRequired-1));
-    }
-    _lastCallback = _now;
+    _lastState = state;
 }
 
 /**
  * Resets the timer on multipresses and (un)pause it on single presses.
  */
 function handleTimerCallback() {
-    if (_multiPressCount >= _multiPressRequired) {
+    switch (_multiPressCount) {
+    case _resetTimerCount:
         // Reset the time and pauses it
         toggleTimer(true);
         _accumulatedTime = 0;
         _prevAccumulatedTime = 0;
         setTimerText();
-    }
-    else {
-        // (Un)Pauses the timer
+        break;
+    default:
         toggleTimer();
+        break;
     }
-
-    _pressHandle = null;
 }
 
 /**
@@ -80,11 +104,14 @@ function toggleTimer(forceHalt) {
     if (forceHalt || _timerRunner != null) {
         window.clearInterval(_timerRunner);
         _timerRunner = null;
+        // Fix drift from when the button was first pressed
+        _accumulatedTime -= Date.now() - _pressTime;
+        setTimerText();
     }
     else if (_timerRunner == null) {
         _timerRunner = window.setInterval(updateTimer, _updateDelay);
         _prevAccumulatedTime = _accumulatedTime;
-        _lastStartTime = Date.now();
+        _lastStartTime = _pressTime;
     }
 }
 
