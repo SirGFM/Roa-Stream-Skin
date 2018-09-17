ba132a01e53c1ca299bf349a20d3a17787b0d858
SirGFM <gfmandaji@hotmail.com>
2018-09-17 00:32:56 -0300

    script: keyboardListener.js: Update view in BG; fix parsing of message

--- script/keyboardListener.js
+++ script/keyboardListener.js
@@ -12,12 +12,12 @@
  * @param{ctx} A context returned by setupNewKeypad
  * @param{msg} A string with the big-endian bit-mask.
  */
-let updateView = function(ctx, msg) {
+let _updateView = function(ctx, msg) {
     let on = 0;
 
     /* Convert the message into a bit-mask for setting keys */
     for (let i in msg) {
-        let d = parseInt(msg[i])
+        let d = parseInt(msg[i], 16);
 
         on = (d << 4 * i) | on;
     }
@@ -37,45 +37,87 @@ let updateView = function(ctx, msg) {
 }
 
 /**
+ * Update the context, reading from its buffer in a BG thread.
+ *
+ * @param{ctx} A context returned by setupNewKeypad
+ */
+let updateView = function(ctx) {
+    while (ctx.buf.length > 0) {
+        let data = ctx.buf.shift();
+        _updateView(ctx, data);
+    }
+}
+
+/**
  * Starts listening to a keylogger, and setup the images it uses for buttons.
  *
  * @param{keys} An array of images, sequenced as the bits from the sender.
  * @param{address} Address of the serving WebSocket key logger.
  */
 function setupNewKeypad(keys, address) {
-    let ctx = {}
+    /* Setup the context with the keys array and with its BG buffer/FIFO */
+    let ctx = {};
     if (!keys) {
         ctx.keys = [];
     }
     else {
         ctx.keys = keys;
     }
-    ctx.ws = new WebSocket(address)
+    ctx.buf = [];
+    ctx.isCleaning = false;
+    ctx.addr = address;
+    ctx.cb = null;
+    ctx.didStart = false;
+
+    /* Setup helper functions for the websocket */
     ctx.open = function(ev) {
         console.log("Opened connection to: " + address);
+        ctx.didStart = true;
     }
     ctx.close = function(ev) {
         console.log("Closed connection with: " + address);
+        ctx.didStart = false;
+        if (!ctx.isCleaning) {
+            /* Connection closed because of an error, restart it! */
+            // TODO Check if from a close and, if not, restart the connection
+        }
     }
     ctx.recv = function(ev) {
-        updateView(ctx, ev.data);
+        ctx.buf.push(ev.data);
     }
     ctx.err = function(ev) {
         console.log("Error sending/receiving message!");
     }
     ctx.clean = function() {
+        ctx.isCleaning = true;
         ctx.ws.close();
+        if (ctx.cb != null) {
+            clearInterval(ctx.cb);
+            ctx.cb = null;
+        }
+    }
+    ctx.start = function() {
+        /* Setup the websocket */
+        ctx.ws = new WebSocket(address);
+        /* Setup all event handlers */
+        try {
+            ctx.ws.addEventListener("open", ctx.open);
+            ctx.ws.addEventListener("close", ctx.close);
+            ctx.ws.addEventListener("message", ctx.recv);
+            ctx.ws.addEventListener("error", ctx.err);
+        } catch (e) {
+            ctx.clean();
+            throw e;
+        }
     }
 
-    try {
-        ctx.ws.addEventListener("open", ctx.open);
-        ctx.ws.addEventListener("close", ctx.close);
-        ctx.ws.addEventListener("message", ctx.recv);
-        ctx.ws.addEventListener("error", ctx.err);
-    } catch (e) {
-        ctx.clean();
-        throw e;
-    }
+    /* Create a BG thread to handle all inputs (removing workload from
+     * ctx.recv()) */
+    ctx.cb = setInterval(updateView, 1000 / 20, ctx);
+
+    /* Start the connection */
+    ctx.start();
+    // TODO If the connection didn't open after a while (10 seconds?), throw an error!
 
     return ctx;
 }
