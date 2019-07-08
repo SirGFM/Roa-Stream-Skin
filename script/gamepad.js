90d08c05957e4c47bb6ae17d6c7fef561312c52b
SirGFM <gfmandaji@hotmail.com>
2019-07-08 11:58:57 -0300

    [WIP] script: gamepad.js: Add PS1 pad skin

--- script/gamepad.js
+++ script/gamepad.js
@@ -8,6 +8,7 @@ let gamepad = function() {
     let _images = [];
     let _active = null;
     let _interval = null;
+    let _defaultSkin = null;
 
     let _xboxSkin = {
         'released': {
@@ -138,7 +139,7 @@ let gamepad = function() {
                 'button': 15
             }
         },
-        'axes': {
+        'axis': {
             'lstick': {
                 'src': '/img/gamepad/x360/stick.png',
                 'width': 18,
@@ -160,6 +161,163 @@ let gamepad = function() {
                 'hh': 17,
                 'hor': 2,
                 'ver': 3
+            }
+        }
+    };
+    /* TODO: Fix position */
+    let _ps1AnalogSkin = {
+        'released': {
+            'src': '/img/gamepad/ps1/released.png',
+            'width': 190,
+            'left': 18,
+            'top': 18
+        },
+        'button': {
+            'l2': {
+                'src': '/img/gamepad/ps1/pressed/l2.png',
+                'width': 42,
+                'height': 20,
+                'left': 22,
+                'top': 18,
+                'button': 4
+            },
+            'l1': {
+                'src': '/img/gamepad/ps1/pressed/l1.png',
+                'width': 34,
+                'height': 14,
+                'left': 62,
+                'top': 28,
+                'button': 6
+            },
+            'r2': {
+                'src': '/img/gamepad/ps1/pressed/r2.png',
+                'width': 42,
+                'height': 20,
+                'left': 156,
+                'top': 18,
+                'button': 5
+            },
+            'r1': {
+                'src': '/img/gamepad/ps1/pressed/r1.png',
+                'width': 34,
+                'height': 14,
+                'left': 122,
+                'top': 28,
+                'button': 7
+            },
+            'select': {
+                'src': '/img/gamepad/ps1/pressed/select.png',
+                'width': 24,
+                'height': 14,
+                'left': 74,
+                'top': 70,
+                'button': 8
+            },
+            'start': {
+                'src': '/img/gamepad/ps1/pressed/start.png',
+                'width': 24,
+                'height': 14,
+                'left': 120,
+                'top': 70,
+                'button': 9
+            },
+            'square': {
+                'src': '/img/gamepad/ps1/pressed/square.png',
+                'width': 26,
+                'height': 26,
+                'left': 146,
+                'top': 66,
+                'button': 3
+            },
+            'triangle': {
+                'src': '/img/gamepad/ps1/pressed/triangle.png',
+                'width': 26,
+                'height': 26,
+                'left': 168,
+                'top': 44,
+                'button': 0
+            },
+            'cross': {
+                'src': '/img/gamepad/ps1/pressed/cross.png',
+                'width': 26,
+                'height': 26,
+                'left': 160,
+                'top': 92,
+                'button': 2
+            },
+            'circle': {
+                'src': '/img/gamepad/ps1/pressed/circle.png',
+                'width': 26,
+                'height': 26,
+                'left': 182,
+                'top': 70,
+                'button': 1
+            },
+        },
+        'axis': {
+            'lstick': {
+                'src': '/img/gamepad/ps1/stick.png',
+                'width': 18,
+                'height': 18,
+                'left': 36,
+                'top': 80,
+                'hw': 25,
+                'hh': 25,
+                'hor': 0,
+                'ver': 1
+            },
+            'rstick': {
+                'src': '/img/gamepad/ps1/stick.png',
+                'width': 18,
+                'height': 18,
+                'left': 126,
+                'top': 112,
+                'hw': 17,
+                'hh': 17,
+                'hor': 2,
+                'ver': 3
+            }
+        }
+        'hat': {
+            'up': {
+                'src': '/img/gamepad/ps1/pressed/up.png',
+                'width': 14,
+                'height': 14,
+                'left': 82,
+                'top': 102,
+                'button': 12,
+                'idx': 5,
+                'val': -0.75
+            },
+            'down': {
+                'src': '/img/gamepad/ps1/pressed/down.png',
+                'width': 14,
+                'height': 14,
+                'left': 82,
+                'top': 126,
+                'button': 13,
+                'idx': 5,
+                'val': 0.75
+            },
+            'left': {
+                'src': '/img/gamepad/ps1/pressed/left.png',
+                'width': 14,
+                'height': 14,
+                'left': 70,
+                'top': 114,
+                'button': 14,
+                'idx': 4,
+                'val': -0.75
+            },
+            'right': {
+                'src': '/img/gamepad/ps1/pressed/right.png',
+                'width': 14,
+                'height': 14,
+                'left': 94,
+                'top': 114,
+                'button': 15,
+                'idx': 4,
+                'val': 0.75
             }
         }
     };
@@ -187,7 +345,16 @@ let gamepad = function() {
     /** Set a callback for updating the buttons */
     function enableGamepad(gamepadId) {
         if (_interval === null && getGamepadCount(getGamepadList()) > 0) {
-            // TODO if def_id == null, detect from gamepad.id
+            /* Configure the gamepad skin */
+            let skin = _defaultSkin;
+            if (!skin) {
+                if (gamepadId.startsWith(xbox_id))
+                    skin = 'xbox';
+                else if (gamepadId.startsWith(ps1_id))
+                    skin = 'ps1';
+            }
+            let parentContent = document.getElementById('gamepad-view');
+            setup(parentContent, skin);
             /* No 'onpress' event, gotta pool... */
             _interval = setInterval(pollGamepad, 1000 / _fps);
         }
@@ -293,8 +460,28 @@ let gamepad = function() {
 
             gamepad.dispatchTimerEvent(i, state);
         }
-        for (let i in _active.axes) {
-            let _axis = _active.axes[i];
+        for (let i in _active.hat) {
+            let _hat = _active.hat[i];
+
+            let state;
+            if (_hat.val > 0)
+                state = _gp.axes[_hat.idx] >= _hat.val;
+            else
+                state = _gp.axes[_hat.idx] <= _hat.val;
+            if (state != _hat.visible) {
+                _hat.visible = state;
+                if (state) {
+                    _hat.img.style.visibility = 'visible';
+                }
+                else {
+                    _hat.img.style.visibility = 'hidden';
+                }
+            }
+
+            gamepad.dispatchTimerEvent(i, state);
+        }
+        for (let i in _active.axis) {
+            let _axis = _active.axis[i];
             let x = _gp.axes[_axis.hor];
             let y = _gp.axes[_axis.ver];
 
@@ -317,6 +504,21 @@ let gamepad = function() {
     });
 
     return {
+        /**
+         * Set a skin as the default one. Otherwise, a skin will be selected
+         * from the gamepad id.
+         *
+         * @param{skin} The name of the skin.
+         */
+        setDefaultSkin(skin) {
+            _defaultSkin = skin;
+        },
+        /**
+         * Configure the view and bind the buttons according to a gamepad skin.
+         *
+         * @param{parentContent} Attach point for the images.
+         * @param{skin} Name of the skin to be used.
+         */
         setup(parentContent, skin='') {
             let _obj = null;
             switch (skin) {
@@ -335,13 +537,15 @@ let gamepad = function() {
             addObject(_obj.released, parentContent).style.visibility = 'visible';
             for (let i in _obj.button)
                 _obj.button[i].img = addObject(_obj.button[i], parentContent);
-            for (let i in _obj.axes) {
-                _obj.axes[i].img = addObject(_obj.axes[i], parentContent);
-                _obj.axes[i].cx = _obj.axes[i].img.offsetLeft;
-                _obj.axes[i].cx += _obj.axes[i].img.width / 2;
-                _obj.axes[i].cy = _obj.axes[i].img.offsetTop;
-                _obj.axes[i].cy += _obj.axes[i].img.height / 2;
+            for (let i in _obj.axis) {
+                _obj.axis[i].img = addObject(_obj.axis[i], parentContent);
+                _obj.axis[i].cx = _obj.axis[i].img.offsetLeft;
+                _obj.axis[i].cx += _obj.axis[i].img.width / 2;
+                _obj.axis[i].cy = _obj.axis[i].img.offsetTop;
+                _obj.axis[i].cy += _obj.axis[i].img.height / 2;
             }
+            for (let i in _obj.hat)
+                _obj.hat[i].img = addObject(_obj.hat[i], parentContent);
 
             _active = _obj;
         },
